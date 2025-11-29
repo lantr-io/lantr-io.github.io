91be5e60511fe87147889291ad6215c59b682ef5
fernweh0 <aleksey.khodakovskiy@gmail.com>
2025-11-29 01:23:25 +0100

    site: orange links

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -20,14 +20,14 @@ export function BlogPosts({ limit }: { limit?: number } = {}) {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col space-y-1 mb-4 group"
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-3">
-              <p className="text-xm text-neutral-500 dark:text-neutral-500 md:w-[120px]">
+              <p className="text-xm1 text-neutral-500 dark:text-neutral-500 md:w-[120px]">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <TitleTag className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <TitleTag className="text-neutral-900 dark:text-neutral-100 tracking-tight group-hover:text-[#FF8C00] transition-colors">
                 {post.metadata.title}
               </TitleTag>
             </div>
